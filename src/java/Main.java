d791dc08289847a1d74d11038da4b76148fd421e
dimirhouse <dimirhouse@gmail.com>
2017-10-21 16:37:20 +0200

    gotowe

--- src/java/Main.java
+++ src/java/Main.java
@@ -1,8 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("huj");
-        System.out.println("dupa");
+
+        System.out.println("dupa123213");
         System.out.println("daadsadsads");
     }
 }
