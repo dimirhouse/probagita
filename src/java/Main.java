091f7f34a808254e4f57be7448d75a7d493b42a7
dimirhouse <dimirhouse@gmail.com>
2017-10-21 16:31:27 +0200

    poprawki

--- src/java/Main.java
+++ src/java/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("huj");
         System.out.println("dupa");
+        System.out.println("daadsadsads");
     }
 }
